6e0a5785d0e4531be7451a1d2e90390e554ed430
Albert Leonardo Pisa <albert.leo93@gmail.com>
2015-08-12 20:10:46 +0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,21 +4,21 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1549152758680448', // your App ID
-        'clientSecret'  : '1426d3e81a01c6f7d887753e6aeed68c', // your App Secret
-        'callbackURL'   : 'http://opensource.petra.ac.id:11010/auth/facebook/callback'
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
+        'callbackURL'   : ''
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'C1atDPj2iojZAI6VLUfOzkwxj',
-        'consumerSecret'    : 'yrJg9t0E24btg9ezWGn6FR9tZwhaC6PJ6tUgFO6h3kYBPpCth9',
-        'callbackURL'       : 'http://opensource.petra.ac.id:11010/auth/twitter/callback'
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
+        'callbackURL'       : ''
     },
 
     'googleAuth' : {
-        'clientID'      : '62980141677-hgu0nvss6aiafmgaqbesj3ge89it5fek.apps.googleusercontent.com',
-        'clientSecret'  : 'CZtegUKbOCUMu77Wi4hJhqBp',
-        'callbackURL'   : 'http://opensource.petra.ac.id:11010/auth/google/callback'
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
     }
 
 };
